c0cfbef3f1a7a7dbf3ca78b9587fa86570990bfb
volpsz <76572284+volpszz@users.noreply.github.com>
2024-07-28 21:03:24 -0300

    Add files via upload

---
+++ Gerador de senhas/Gerador de Senhas.py
@@ -0,0 +1,21 @@
+import random
+import string
+ 
+
+def gerar_senha(tamanho):
+
+    # Define os caracteres permitidos
+
+    caracteres = string.ascii_letters + string.digits + string.punctuation 
+    senha = ''.join(random.choices(caracteres, k=tamanho))
+
+    return senha
+
+# Solicita o tamanho da senha ao usuário
+
+tamanho = int(input("Digite o tamanho da senha: "))
+
+# Gera e exibe a senha
+
+
+print("Sua senha gerada é:", gerar_senha(tamanho))  
